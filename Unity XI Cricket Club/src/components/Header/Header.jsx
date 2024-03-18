58bedeca916a990eaf10b202be47682c1c4cdb15
Udaykumar Dhokia <udaykumardhokia@gmail.com>
2024-03-18 23:19:41 +0530

    Initial

---
+++ Unity XI Cricket Club/src/components/Header/Header.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "../Header/Header.css"
+import logo from "../../assets/logo.png"
+
+const Header = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+          <a href="/" className="navbar-brand"><img src={logo} className="logo"/></a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+          <i class="bi bi-list"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Achievements
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Statistics
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Players
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Club
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="" aria-disabled="true">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
