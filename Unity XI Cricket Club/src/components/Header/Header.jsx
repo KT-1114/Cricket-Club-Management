1885676a510a3b0fc7a029674cea9b5ea59c3488
Udaykumar Dhokia <udaykumardhokia@gmail.com>
2024-03-21 23:28:51 +0530

    Second

--- Unity XI Cricket Club/src/components/Header/Header.jsx
+++ Unity XI Cricket Club/src/components/Header/Header.jsx
@@ -5,9 +5,60 @@ import logo from "../../assets/logo.png"
 const Header = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark">
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-bottom d-sm-block d-none">
         <div class="container">
-          <a href="/" className="navbar-brand"><img src={logo} className="logo"/></a>
+          <a href="/" className="navbar-brand animate__animated animate__fadeIn"><img src={logo} className="logo"/></a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+          <i class="bi bi-list"></i>
+          </button>
+          <div class="collapse navbar-collapse animate__animated animate__fadeInRight" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Achievements
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Statistics
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Players
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Club
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="" aria-disabled="true">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <nav class="navbar navbar-expand-lg navbar-dark d-block d-sm-none">
+        <div class="container">
+          <a href="/" className="navbar-brand animate__animated animate__fadeIn"><img src={logo} className="logo"/></a>
           <button
             class="navbar-toggler"
             type="button"
@@ -22,7 +73,7 @@ const Header = () => {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">
+                <a class="nav-link" aria-current="page" href="/">
                   Home
                 </a>
               </li>
--- Unity XI Cricket Club/src/pages/Home/Homepage.jsx
+++ Unity XI Cricket Club/src/pages/Home/Homepage.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
+import React from "react";
+import Header from "../../components/Header/Header";
+import "../Home/Homepage.css";
 
 const Homepage = () => {
   return (
     <div>
-        <Header/>
-    </div>
-  )
-}
+      <Header />
+      <section>
+        <div class="container-fluid main px-4 px-lg-5 animate__animated animate__fadeInLeft">
+          <div class="row gx-4 gx-lg-5">
+            <div class="col-lg-7">
+              <h1 class="mt-5 text-white">Unity XI Cricket Club</h1>
+              <p className="text-white">
+                Established in 2015, Unity XI Cricket Club has emerged as a
+                vibrant hub for cricket enthusiasts in the local community.
+                Founded on the principles of camaraderie, sportsmanship, and
+                passion for the game, our club has become a cornerstone of
+                cricketing excellence in the region.
+              </p>
+              <p className="text-white">
+                <strong> Mission Statement: </strong>
+                <br />
+                Unity XI Cricket Club is committed to fostering a strong sense
+                of unity and teamwork while promoting the values of fair play,
+                dedication, and respect for the game. Through our inclusive and
+                supportive environment, we aim to provide opportunities for
+                players of all skill levels to develop and thrive in their
+                cricketing journey.
+              </p>
+              <p className="text-white">
+                <strong> Vision for the Future:</strong>
+                <br />
+                Looking ahead, Unity XI Cricket Club is committed to continuing
+                its legacy of excellence and service to the community. We aspire
+                to further expand our outreach programs, enhance our facilities,
+                and cultivate a culture of continuous learning and improvement.
+                With the unwavering support of our members, volunteers, and
+                stakeholders, we are confident in our ability to make a lasting
+                impact on the world of cricket and beyond.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
-export default Homepage
+      <section>
+        <div class="container-fluid achieve px-4 px-lg-5 animate__animated">
+          <div class="row gx-4 gx-lg-5">
+            <div class="col-lg-7">
+              <h1 class="mt-5 text-dark">Recent Achievement</h1>
+              <p className="text-dark">
+                Uniting Passion, Excellence, and Community. Our club is
+                dedicated to fostering a love for cricket while promoting
+                teamwork, skill development, and sportsmanship. Join us for
+                thrilling matches, camaraderie, and the joy of playing the
+                beloved sport of cricket.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Homepage;
